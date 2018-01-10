bfa56b9539442ff2a6d56b69f3dff099980bbe27
Morgan Intrator <morgan.intrator@gmail.com>
2018-01-10 14:01:59 -0500

    Temporary?? Update to compile css

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -241,106 +241,121 @@ module.exports = {
           // in the main CSS file.
           {
             test: /\.global\.s?css$/,
-            use: [
-              {
-                loader: 'style-loader',
-                options: {
-                  sourceMap: true,
-                  insertAt: 'top',
-                  // combine all <link rel="stylesheet" href="blob:..."> tags into one
-                  singleton: true,
-                }
-              },
-              {
-                loader: require.resolve('css-loader'),
-                options: {
-                  importLoaders: 1,
-                  minimize: true,
-                  sourceMap: true,
+            loader: ExtractTextPlugin.extract(
+              Object.assign(
+                {
+                  fallback: require.resolve('style-loader'),
+                  use: [
+                    // {
+                    //   loader: 'style-loader',
+                    //   options: {
+                    //     sourceMap: true,
+                    //     insertAt: 'top',
+                    //     // combine all <link rel="stylesheet" href="blob:..."> tags into one
+                    //     singleton: true,
+                    //   }
+                    // },
+                    {
+                      loader: require.resolve('css-loader'),
+                      options: {
+                        importLoaders: 1,
+                        minimize: true,
+                        sourceMap: true,
+                      },
+                    },
+                    {
+                      loader: require.resolve('postcss-loader'),
+                      options: {
+                        // Necessary for external CSS imports to work
+                        // https://github.com/facebookincubator/create-react-app/issues/2677
+                        ident: 'postcss',
+                        sourceMap: true,
+                        plugins: () => [
+                          require('postcss-flexbugs-fixes'),
+                          autoprefixer({
+                            browsers: [
+                              '>1%',
+                              'last 4 versions',
+                              'Firefox ESR',
+                              'not ie < 9', // React doesn't support IE8 anyway
+                            ],
+                            flexbox: 'no-2009',
+                          }),
+                        ],
+                      },
+                    },
+                    {
+                      loader: require.resolve('sass-loader'),
+                      options: {
+                        sourceMap: true
+                      }
+                    }]
                 },
-              },
-              {
-                loader: require.resolve('postcss-loader'),
-                options: {
-                  // Necessary for external CSS imports to work
-                  // https://github.com/facebookincubator/create-react-app/issues/2677
-                  ident: 'postcss',
-                  sourceMap: true,
-                  plugins: () => [
-                    require('postcss-flexbugs-fixes'),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }),
-                  ],
-                },
-              },
-              {
-                loader: require.resolve('sass-loader'),
-                options: {
-                  sourceMap: true
-                }
-              },
-            ],
+                extractTextPluginOptions
+              )
+            )
           },
           {
             test: /\.s?css$/,
             exclude: /\.global\.s?css$/,
-            use: [
-              {
-                loader: 'style-loader',
-                options: {
-                  sourceMap: true,
-                  insertAt: 'top',
-                  // combine all <link rel="stylesheet" href="blob:..."> tags into one
-                  singleton: true,
-                }
-              },
-              {
-                loader: require.resolve('css-loader'),
-                options: {
-                  importLoaders: 1,
-                  modules: true,
-                  minimize: true,
-                  camelCase: true,
-                  autoprefixer: false,
-                  sourceMap: true,
-                  localIdentName: 'bw__[name]__[local]',
+            loader: ExtractTextPlugin.extract(
+              Object.assign(
+                {
+                  fallback: require.resolve('style-loader'),
+                  use: [
+                    // {
+                    //   loader: 'style-loader',
+                    //   options: {
+                    //     sourceMap: true,
+                    //     insertAt: 'top',
+                    //     // combine all <link rel="stylesheet" href="blob:..."> tags into one
+                    //     singleton: true,
+                    //   }
+                    // },
+                    {
+                      loader: require.resolve('css-loader'),
+                      options: {
+                        importLoaders: 1,
+                        modules: true,
+                        minimize: true,
+                        camelCase: true,
+                        autoprefixer: false,
+                        sourceMap: true,
+                        localIdentName: 'bw__[name]__[local]',
+                      },
+                    },
+                    {
+                      loader: require.resolve('postcss-loader'),
+                      options: {
+                        // Necessary for external CSS imports to work
+                        // https://github.com/facebookincubator/create-react-app/issues/2677
+                        ident: 'postcss',
+                        sourceMap: true,
+                        plugins: () => [
+                          require('postcss-flexbugs-fixes'),
+                          autoprefixer({
+                            browsers: [
+                              '>1%',
+                              'last 4 versions',
+                              'Firefox ESR',
+                              'not ie < 9', // React doesn't support IE8 anyway
+                            ],
+                            flexbox: 'no-2009',
+                          }),
+                        ],
+                      },
+                    },
+                    {
+                      loader: require.resolve('sass-loader'),
+                      options: {
+                        sourceMap: true
+                      }
+                    },
+                  ]
                 },
-              },
-              {
-                loader: require.resolve('postcss-loader'),
-                options: {
-                  // Necessary for external CSS imports to work
-                  // https://github.com/facebookincubator/create-react-app/issues/2677
-                  ident: 'postcss',
-                  sourceMap: true,
-                  plugins: () => [
-                    require('postcss-flexbugs-fixes'),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }),
-                  ],
-                },
-              },
-              {
-                loader: require.resolve('sass-loader'),
-                options: {
-                  sourceMap: true
-                }
-              },
-            ],
+                extractTextPluginOptions
+              )
+            )
           },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
@@ -411,9 +426,9 @@ module.exports = {
       sourceMap: shouldUseSourceMap,
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
-    // new ExtractTextPlugin({
-    //   filename: cssFilename,
-    // }),
+    new ExtractTextPlugin({
+      filename: cssFilename,
+    }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
